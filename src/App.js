837ea811b19d3c58dd04396c96be51e4de7495ac
И Марк <imark.work.ac@gmail.com>
2021-09-04 23:43:15 +0300

    checking error component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { Component } from "react";
-import "./App.css";
+import Counter from "./Counter";
+import React, { Component, useState } from "react";
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      num: 0,
+      toggle: true,
+      ignoreProp: 0,
+      seed: 40,
+      showErrorTab: true,
     };
   }
 
+  toggleApp = () => {
+    this.setState({
+      toggle: !this.state.toggle,
+    });
+  };
+
+  ignoreProp = () => {
+    this.setState({
+      ignoreProp: Math.random(),
+    });
+  };
+
+  generateSeed = () => {
+    this.setState({
+      seed: parseInt(Math.random() * 100),
+    });
+  };
+
+  showErrorTab = () => {
+    this.setState({
+      showErrorTab: !this.state.showErrorTab
+    });
+  };
+
   render() {
-    return <div className="App"></div>;
+    return (
+      <div>
+        {this.state.toggle ? <Counter showErrorTab={this.state.showErrorTab} seed={this.state.seed} ignoreProp={this.state.ignoreProp} /> : null}
+        {/* {toggle && <Counter />} */}
+        <button onClick={this.toggleApp}>Toggle App</button>
+        <button onClick={this.ignoreProp}>Ignore Prop</button>
+        <button onClick={this.generateSeed}>Generate Seed</button>
+        <button onClick={this.showErrorTab}>Show Error</button>
+      </div>
+    );
   }
 }
-
-export default App;
