bc34c04d9d9ef8a6d2d3600d754d931b0a4e33b5
И Марк <imark.work.ac@gmail.com>
2021-09-04 12:31:41 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      num: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
