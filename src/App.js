8f48989a5ef957cc61c2171810f30a4e1fbbbd9d
И Марк <imark.work.ac@gmail.com>
2021-09-05 20:56:34 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
@@ -6,12 +6,108 @@ class App extends Component {
     super(props);
 
     this.state = {
-      num: 0,
+      value: 2,
     };
   }
 
+  changeSeason = (e) => {
+    this.setState({
+      value: +e.target.value,
+    });
+  };
+
   render() {
-    return <div className="App"></div>;
+    const value = this.state.value;
+    // if (value === 1) {
+    //   return (
+    //     <div className="season">
+    //       <input type="number" min="1" max="4" onClick={this.changeSeason} />
+    //       <img src="winter.jpg" alt="" />
+    //       <h1>Зима</h1>
+    //     </div>
+    //   );
+    // } else if (value === 2) {
+    //   return (
+    //     <div className="season">
+    //       <input type="number" min="1" max="4" onClick={this.changeSeason} />
+    //       <img src="spring.jpg" alt="" />
+    //       <h1>Весна</h1>
+    //     </div>
+    //   );
+    // } else if (value === 3) {
+    //   return (
+    //     <div className="season">
+    //       <input type="number" min="1" max="4" onClick={this.changeSeason} />
+    //       <img src="summer.jpg" alt="" />
+    //       <h1>Лето</h1>
+    //     </div>
+    //   );
+    // } else {
+    //   return (
+    //     <div className="season">
+    //       <input type="number" min="1" max="4" onClick={this.changeSeason} />
+    //       <img src="autumn.jpg" alt="" />
+    //       <h1>Осень</h1>
+    //     </div>
+    //   );
+    // }
+
+    return (
+      <div>
+        <input type="number" min="1" max="4" onClick={this.changeSeason} />
+        <img
+          src={
+            value === 1
+              ? "winter.jpg"
+              : value === 2
+              ? "spring.jpg"
+              : value === 3
+              ? "summer.jpg"
+              : "autumn.jpg"
+          }
+          alt=""
+        />
+        <div>
+          {value === 1
+            ? "Зима"
+            : value === 1
+            ? "Весна"
+            : value === 1
+            ? "Лето"
+            : "Осень"}
+        </div>
+      </div>
+    );
+
+    // return (
+    //   <div className="season">
+    //     <input type="number" min="1" max="4" onClick={this.changeSeason}/>
+    //     {value === 1 && (
+    //       <Fragment>
+    //         <img src="winter.jpg" alt="" />
+    //         <h1>Зима</h1>
+    //       </Fragment>
+    //     )}
+    //     {value === 2 && (
+    //       <Fragment>
+    //         <img src="spring.jpg" alt="" />
+    //         <h1>Весна</h1>
+    //       </Fragment>
+    //     )}
+    //     {value === 3 && (
+    //       <Fragment>
+    //         <img src="summer.jpg" alt="" />
+    //         <h1>Лето</h1>
+    //       </Fragment>
+    //     )}
+    //     {value === 4 && (
+    //       <Fragment>
+    //         <img src="autumn.jpg" alt="" />
+    //         <h1>Осень</h1>
+    //       </Fragment>
+    //     )}
+    //   </div>
+    // );
   }
 }
 
